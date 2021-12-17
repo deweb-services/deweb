cfa5a9802f487018cf81ffa2383f861e9abab495
berelton <berelton@pm.me>
2021-12-17 12:34:42 -0600

    vue fix

--- vue/src/store/generated/deweb-services/deweb/dewebservices.deweb.deweb/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { UserKeyRec } from "./module/types/deweb/keyrec";
-import { RecordsToUser } from "./module/types/deweb/keyrec";
-export { UserKeyRec, RecordsToUser };
-declare const _default;
-export default _default;
