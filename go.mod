2085c4e445f68f072a13c58409236eb9e5df3a94
borispahomov <borishome1287@mail.ru>
2022-08-30 23:22:46 +0300

    DNS proxy if not found in chain

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
-	github.com/miekg/dns v1.1.43
+	github.com/miekg/dns v1.1.50
 	github.com/spf13/cast v1.5.0
 	github.com/spf13/cobra v1.4.0
 	github.com/spf13/pflag v1.0.5
@@ -19,9 +19,10 @@ require (
 	github.com/tendermint/starport v0.19.2
 	github.com/tendermint/tendermint v0.34.19
 	github.com/tendermint/tm-db v0.6.7
-	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
 	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f // indirect
 	golang.org/x/sys v0.0.0-20220610221304-9f5ed59c137d // indirect
+	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
 	google.golang.org/genproto v0.0.0-20220822174746-9e6da59bd2fc
 	google.golang.org/grpc v1.48.0
 	gopkg.in/yaml.v2 v2.4.0
--- x/dns_server/dnsserver.go
+++ x/dns_server/dnsserver.go
@@ -1,45 +1,14 @@
 package dns_server
 
 import (
-	"bytes"
-	"context"
-	"encoding/binary"
 	"fmt"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/deweb-services/deweb/x/dns_module/types"
 	"github.com/miekg/dns"
-	"golang.org/x/net/dns/dnsmessage"
 	"net"
 	"os"
+	"regexp"
 	"time"
-)
-
-// DNSHeader describes the request/response DNS header
-type DNSHeader struct {
-	TransactionID  uint16
-	Flags          uint16
-	NumQuestions   uint16
-	NumAnswers     uint16
-	NumAuthorities uint16
-	NumAdditionals uint16
-}
-
-// DNSResourceRecord describes individual records in the request and response of the DNS payload body
-type DNSResourceRecord struct {
-	DomainName         string
-	Type               uint16
-	Class              uint16
-	TimeToLive         uint32
-	ResourceDataLength uint16
-	ResourceData       []byte
-}
-
-// Type and Class values for DNSResourceRecord
-const (
-	TypeA                  uint16 = 1
-	TypeMX                 uint16 = 15
-	ClassINET              uint16 = 1   // the Internet
-	UDPMaxMessageSizeBytes uint   = 512 // RFC1035
 )
 
 type CacheRecord struct {
@@ -64,226 +33,6 @@ func NewDNSResolverService(cliCtx client.Context, proxyServer string) *DNSResolv
 	}
 }
 
-// Pretend to look up values in a database
-func (srv *DNSResolverService) dbLookup(queryResourceRecord DNSResourceRecord) []DNSResourceRecord {
-	var answerResourceRecords = make([]DNSResourceRecord, 0)
-
-	resolvedRecords, err := srv.resolveDNSRecord(queryResourceRecord.DomainName, queryResourceRecord.Type)
-	if err != nil {
-		fmt.Printf("Cannot resolve record for %s (%d): %v", queryResourceRecord.DomainName, queryResourceRecord.Type, err)
-		proxyResourceRecords := srv.proxyRequest(queryResourceRecord)
-		return proxyResourceRecords
-	}
-	for _, rec := range resolvedRecords {
-		var resData []byte
-		if queryResourceRecord.Type == 1 {
-			ipAddr := net.ParseIP(rec)
-			resData = ipAddr[12:16]
-		} else {
-			resData = []byte(rec)
-		}
-
-		answerResourceRecords = append(answerResourceRecords, DNSResourceRecord{
-			DomainName:         queryResourceRecord.DomainName,
-			Type:               queryResourceRecord.Type,
-			Class:              ClassINET,
-			TimeToLive:         31337,
-			ResourceData:       resData, // ipv4 address
-			ResourceDataLength: uint16(len(resData)),
-		})
-	}
-
-	return answerResourceRecords
-}
-
-func (srv *DNSResolverService) resolveDNSRecord(domain string, recordType uint16) ([]string, error) {
-	queryClient := types.NewQueryClient(srv.cliCtx)
-	resp, err := queryClient.Domain(
-		context.Background(),
-		&types.QueryDomainRequest{
-			DomainName: domain,
-		},
-	)
-	if err != nil {
-		return nil, fmt.Errorf("cannot perform NFT search: %w", err)
-	}
-
-	var storedRecords []*types.DNSRecords
-	recFromCache, ok := srv.cachedRecords[domain]
-	if ok {
-		cacheExpireTime := recFromCache.CreateTime.Add(10 * time.Second)
-		if cacheExpireTime.After(time.Now()) {
-			storedRecords = recFromCache.Values
-		} else {
-			delete(srv.cachedRecords, domain)
-		}
-	}
-	if storedRecords == nil {
-		srv.cachedRecords[domain] = CacheRecord{
-			Values:     resp.Domain.Records,
-			CreateTime: time.Now(),
-		}
-		storedRecords = resp.Domain.Records
-	}
-
-	recordTypeName, ok := recordTypesMapping[recordType]
-	if !ok {
-		return nil, fmt.Errorf("unsupported type %d", recordType)
-	}
-	for _, rec := range storedRecords {
-		if rec.Type == recordTypeName {
-			return rec.Values, nil
-		}
-	}
-	return nil, fmt.Errorf("record for %s not found", domain)
-}
-
-// RFC1035: "Domain names in messages are expressed in terms of a sequence
-// of labels. Each label is represented as a one octet length field followed
-// by that number of octets.  Since every domain name ends with the null label
-// of the root, a domain name is terminated by a length byte of zero."
-func (srv *DNSResolverService) readDomainName(requestBuffer *bytes.Buffer) (string, error) {
-	var domainName string
-
-	b, err := requestBuffer.ReadByte()
-
-	for ; b != 0 && err == nil; b, err = requestBuffer.ReadByte() {
-		labelLength := int(b)
-		labelBytes := requestBuffer.Next(labelLength)
-		labelName := string(labelBytes)
-
-		if len(domainName) == 0 {
-			domainName = labelName
-		} else {
-			domainName += "." + labelName
-		}
-	}
-
-	return domainName, err
-}
-
-func (srv *DNSResolverService) prepareDNSAnswer(reqID uint16, dnsQuestions []DNSResourceRecord, dnsAnswers []DNSResourceRecord) ([]byte, error) {
-	msg := dnsmessage.Message{
-		Header: dnsmessage.Header{
-			Response:      true,
-			Authoritative: true,
-			ID:            reqID,
-		},
-		Questions: make([]dnsmessage.Question, 0, len(dnsQuestions)),
-		Answers:   make([]dnsmessage.Resource, 0, len(dnsAnswers)),
-	}
-	for _, qRec := range dnsQuestions {
-		domainName := qRec.DomainName + "."
-		name, err := dnsmessage.NewName(domainName)
-		if err != nil {
-			continue
-		}
-		qMessage := dnsmessage.Question{
-			Name:  name,
-			Type:  dnsmessage.Type(qRec.Type),
-			Class: dnsmessage.ClassINET,
-		}
-		msg.Questions = append(msg.Questions, qMessage)
-	}
-
-	for _, answer := range dnsAnswers {
-		domainName := answer.DomainName + "."
-		name, err := dnsmessage.NewName(domainName)
-		if err != nil {
-			continue
-		}
-		var body dnsmessage.ResourceBody
-		switch answer.Type {
-		case TypeA:
-			if len(answer.ResourceData) != 4 {
-				fmt.Printf("Invalid response with length %d: %v\n", len(answer.ResourceData), answer.ResourceData)
-			}
-			var resContent [4]byte
-			for i := 0; i < 4; i++ {
-				resContent[i] = answer.ResourceData[i]
-			}
-			//resContent = [4]byte{192, 168, 1, 1}
-			body = &dnsmessage.AResource{A: resContent}
-		case TypeMX:
-			resName, err := dnsmessage.NewName(string(answer.ResourceData))
-			if err != nil {
-				fmt.Printf("cannot process MX response for %s. result=%s: %v",
-					answer.DomainName, string(answer.ResourceData), err)
-			}
-			body = &dnsmessage.MXResource{
-				Pref: 0,
-				MX:   resName,
-			}
-		}
-
-		answerRec := dnsmessage.Resource{
-			Header: dnsmessage.ResourceHeader{
-				Name:  name,
-				Type:  dnsmessage.Type(answer.Type),
-				Class: dnsmessage.ClassINET,
-			},
-			Body: body,
-		}
-		msg.Answers = append(msg.Answers, answerRec)
-	}
-
-	buf, err := msg.Pack()
-	if err != nil {
-		return nil, fmt.Errorf("cannot pack message: %w", err)
-	}
-	return buf, nil
-}
-
-func (srv *DNSResolverService) handleDNSClient(requestBytes []byte, serverConn *net.UDPConn, clientAddr *net.UDPAddr) {
-	/**
-	 * read request
-	 */
-	var requestBuffer = bytes.NewBuffer(requestBytes)
-	var queryHeader DNSHeader
-	var queryResourceRecords []DNSResourceRecord
-
-	err := binary.Read(requestBuffer, binary.BigEndian, &queryHeader) // network byte order is big endian
-
-	if err != nil {
-		fmt.Println("Error decoding header: ", err.Error())
-	}
-
-	queryResourceRecords = make([]DNSResourceRecord, queryHeader.NumQuestions)
-
-	for idx, _ := range queryResourceRecords {
-		queryResourceRecords[idx].DomainName, err = srv.readDomainName(requestBuffer)
-
-		if err != nil {
-			fmt.Println("Error decoding label: ", err.Error())
-		}
-
-		queryResourceRecords[idx].Type = binary.BigEndian.Uint16(requestBuffer.Next(2))
-		queryResourceRecords[idx].Class = binary.BigEndian.Uint16(requestBuffer.Next(2))
-	}
-
-	/**
-	 * lookup values
-	 */
-	var answerResourceRecords = make([]DNSResourceRecord, 0)
-
-	for _, queryResourceRecord := range queryResourceRecords {
-		newAnswerRR := srv.dbLookup(queryResourceRecord)
-
-		answerResourceRecords = append(answerResourceRecords, newAnswerRR...) // three dots cause the two lists to be concatenated
-	}
-
-	/**
-	 * write response
-	 */
-
-	resBytes, err := srv.prepareDNSAnswer(queryHeader.TransactionID, queryResourceRecords, answerResourceRecords)
-	if err != nil {
-		fmt.Printf("Error making response for %v: %v", queryResourceRecords, err)
-		resBytes = make([]byte, 0)
-	}
-	_, _ = serverConn.WriteToUDP(resBytes, clientAddr)
-}
-
 func (srv *DNSResolverService) RunServer(port int) {
 	serverAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", port))
 
@@ -292,63 +41,36 @@ func (srv *DNSResolverService) RunServer(port int) {
 		os.Exit(1)
 	}
 
-	serverConn, err := net.ListenUDP("udp", serverAddr)
+	dns.HandleFunc(".", func(w dns.ResponseWriter, r *dns.Msg) {
+		switch r.Opcode {
+		case dns.OpcodeQuery:
+			m, err := srv.getResponse(r)
+			if err != nil {
+				fmt.Printf("Failed lookup for %s with error: %s\n", r, err.Error())
+				m.SetReply(r)
+				w.WriteMsg(m)
+				return
+			}
+			if len(m.Answer) > 0 {
+				pattern := regexp.MustCompile(`(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}`)
+				ipAddress := pattern.FindAllString(m.Answer[0].String(), -1)
 
-	if err != nil {
-		fmt.Println("Error listening: ", err.Error())
-		os.Exit(1)
-	}
-
-	fmt.Println("DNS Listening at: ", serverAddr)
-
-	defer serverConn.Close()
-
-	for {
-		requestBytes := make([]byte, UDPMaxMessageSizeBytes)
-
-		_, clientAddr, err := serverConn.ReadFromUDP(requestBytes)
-
-		if err != nil {
-			fmt.Println("Error receiving: ", err.Error())
-		} else {
-			fmt.Println("Received request from ", clientAddr)
-			go srv.handleDNSClient(requestBytes, serverConn, clientAddr) // array is value type (call-by-value), i.e. copied
+				if len(ipAddress) > 0 {
+					fmt.Printf("Lookup for %s with ip %s\n", m.Answer[0].Header().Name, ipAddress[0])
+				} else {
+					fmt.Printf("Lookup for %s with response %s\n", m.Answer[0].Header().Name, m.Answer[0])
+				}
+			}
+			m.SetReply(r)
+			w.WriteMsg(m)
 		}
-	}
-}
+	})
 
-func (srv *DNSResolverService) proxyRequest(queryResourceRecord DNSResourceRecord) []DNSResourceRecord {
-	dnsProxy := DNSProxy{
-		defaultServer: srv.proxyDNSServer,
-	}
-	reqDomain := queryResourceRecord.DomainName + "."
-	reqQuestion := dns.Question{
-		Name:   reqDomain,
-		Qtype:  queryResourceRecord.Type,
-		Qclass: queryResourceRecord.Class,
-	}
-	reqMsg := &dns.Msg{
-		Question: []dns.Question{reqQuestion},
-	}
-	results := make([]DNSResourceRecord, 0)
-	resp, err := dnsProxy.getResponse(reqMsg)
+	server := &dns.Server{Addr: serverAddr.String(), Net: "udp"}
+	fmt.Printf("Starting at %s\n", serverAddr.String())
+	err = server.ListenAndServe()
 	if err != nil {
-		return results
+		fmt.Printf("Failed to start server: %s\n ", err.Error())
 	}
-	for _, resRec := range resp.Answer {
-		resHeader := resRec.Header()
-		aRec := resRec.(*dns.A)
-		ipAddr := aRec.A
-		resData := []byte(ipAddr)
-		resDNSRec := DNSResourceRecord{
-			DomainName:         queryResourceRecord.DomainName,
-			Type:               resHeader.Rrtype,
-			Class:              resHeader.Class,
-			TimeToLive:         resHeader.Ttl,
-			ResourceDataLength: resHeader.Rdlength,
-			ResourceData:       resData,
-		}
-		results = append(results, resDNSRec)
-	}
-	return results
+
 }
